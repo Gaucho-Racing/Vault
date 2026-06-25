4196ae9c4b90df598bf0d3416def1f19b6e9ab4f
Bharat Kathi <bharat.kathi@withpersona.com>
2026-06-25 14:27:02 -0700

    feat: add Gaucho Racing footer

--- web/src/components/AppShell.tsx
+++ web/src/components/AppShell.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 
+import { AppFooter } from "@/components/AppFooter"
 import { AppHeader } from "@/components/AppHeader"
 import { AppSidebar } from "@/components/AppSidebar"
 import { TooltipProvider } from "@/components/ui/tooltip"
@@ -9,9 +10,12 @@ export function AppShell() {
     <TooltipProvider>
       <div className="grid min-h-svh bg-background text-foreground lg:grid-cols-[264px_minmax(0,1fr)]">
         <AppSidebar />
-        <div className="min-w-0 bg-background">
+        <div className="flex min-h-svh min-w-0 flex-col bg-background">
           <AppHeader />
-          <Outlet />
+          <main className="flex-1">
+            <Outlet />
+          </main>
+          <AppFooter />
         </div>
       </div>
     </TooltipProvider>
